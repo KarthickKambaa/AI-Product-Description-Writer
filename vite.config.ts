78f3f9623b2527a24a0b3ee80d7317c9c858c19f
Karthick <120363078+KarthickKambaa@users.noreply.github.com>
2025-04-10 23:01:52 +0530

    initial commit
    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: true
+  },
+  server: {
+    port: 3000
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
